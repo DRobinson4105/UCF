edaf0d00c19d5835f769a27ae87d3a82ee8c9093
DRobinson4105 <58113704+DRobinson4105@users.noreply.github.com>
2023-02-19 23:22:40 -0500

    Completed Most of 2022 Practice Problems
    Completed all problems except for naming and artifacts

---
+++ UCFHSPT/artifacts.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int numExpeditions;
+    cin >> numExpeditions;
+    
+    for(int expedition = 0; expedition < numExpeditions; expedition++)
+    {
+        
+    }
+
+    return 0;
+}
