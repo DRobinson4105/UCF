6f53e44a2f7e6919d262b43dc7b0539262f8453c
David Robinson <drobinson4105@gmail.com>
2023-04-16 09:57:58 -0400

    Fix CS1 homework

--- Schoolwork/COP3502/assignment7_Robinson_David.c
+++ Schoolwork/COP3502/assignment7_Robinson_David.c
@@ -1,9 +1,5 @@
-// This program demonstrates how to implement
-// some common priority queue / heap functions
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 
 // Macros for finding parent and children indices
 #define PARENT(index)  (((index)-1)/2)
@@ -21,6 +17,7 @@ struct Shipment {
     int expiration; // expiration time
     double mass; // shipment mass
 };
+
 struct Heap {
     Shipment * array; // array to hold elements in heap
     int size; // current number of elements in heap
@@ -83,7 +80,7 @@ int main()
     int numShipments; // number of shipments
     int start, end; // starting and ending eating time of the crew
     double lowerBound, upperBound, mid; // used for binary search
-    int totalMass = 0;
+    int totalMass = 0; // track total mass when inputing shipments
 
     scanf("%d", &numShipments);
 
@@ -91,8 +88,7 @@ int main()
     shipments = (Shipment *) malloc(sizeof(Shipment) * numShipments);
 
     // Get input for all shipments
-    for (int i = 0; i < numShipments; i++)
-    {
+    for (int i = 0; i < numShipments; i++) {
         scanf("%d %d %lf", &shipments[i].arrival, &shipments[i].expiration, &shipments[i].mass);
         totalMass += shipments[i].mass;
     }
@@ -100,11 +96,10 @@ int main()
     scanf("%d %d", &start, &end);
 
     // Clamp all shipments to start and end eating time
-    for (int i = 0; i < numShipments; i++)
-    {
-        // If shipment would be invalid by expiring before start or arriving after end
-        if (shipments[i].expiration <= start || shipments[i].arrival >= end)
-        {
+    for (int i = 0; i < numShipments; i++) {
+        // If shipment would be invalid by expiring before
+        // start or arriving after end, remove it from array
+        if (shipments[i].expiration <= start || shipments[i].arrival >= end) {
             totalMass -= shipments[i].mass;
             i--;
             numShipments--;
@@ -127,12 +122,15 @@ int main()
     upperBound = totalMass / (end - start);
 
     // Binary search until valid bound has an absolute error of 10^-5
-    while (upperBound - lowerBound > 1e-5)
-    {
+    while (upperBound - lowerBound > 1e-5) {
         mid = (lowerBound + upperBound) / 2;
 
+        // If the crew could survive at the guessed consumption rate,
+        // see if the crew can survive at a lower consume rate
         if (canDo(shipments, numShipments, mid, start, end))
             lowerBound = mid;
+
+        // Otherwise, the consumption rate needs to be higher
         else
             upperBound = mid;
     }
@@ -142,8 +140,7 @@ int main()
     return 0;
 }
 
-void merge(Shipment * array, int low, int mid, int high)
-{
+void merge(Shipment * array, int low, int mid, int high) {
     int length1 = mid - low + 1;
     int length2 = high - mid;
 
@@ -158,13 +155,12 @@ void merge(Shipment * array, int low, int mid, int high)
     for(int i = 0; i < length2; i++)
         arrR[i] = array[mid + 1 + i];
 
-    int i = 0; // Initial index of first subarray
-    int j = 0; // Initial index of second subarray
-    int k = low; // Initial index of merged subarray
+    int i = 0; // initial index of first subarray
+    int j = 0; // initial index of second subarray
+    int k = low; // initial index of merged subarray
     
     // Merge temp arrays back into array[low..high]
-    while (i < length1 && j < length2)
-    {
+    while (i < length1 && j < length2) {
         // Add the lower arrival time to merged array
         if (arrL[i].arrival <= arrR[j].arrival)
             array[k++] = arrL[i++];
@@ -185,8 +181,7 @@ void merge(Shipment * array, int low, int mid, int high)
     free(arrR);
 }
 
-void mergeSort(Shipment * array, int low, int high)
-{
+void mergeSort(Shipment * array, int low, int high) {
     // If size of subarray is 0 or 1, do nothing
     if(low >= high) return;
 
@@ -233,25 +228,21 @@ void percolateUp(Heap * hp, int index) {
         int parIndex = PARENT(index);
 
         // Check if current element is higher priority than parent
-        if (hp->array[index].expiration < hp->array[parIndex].expiration)
-        {
+        if (hp->array[index].expiration < hp->array[parIndex].expiration) {
             // Swap to the parent
             swap(hp, index, parIndex);
             index = parIndex;
-        } 
-        
+        }
+
         // Done percolating
-        else
-            return;
+        else return;
     }
 }
 
-void percolateDown(Heap * hp, int index)
-{
+void percolateDown(Heap * hp, int index) {
     int swapped = 1;
     // Loop until there are no more swaps
-    while (swapped)
-    {
+    while (swapped) {
         // Set that there are no swaps for the current iteration
         swapped = 0;
 
@@ -271,8 +262,7 @@ void percolateDown(Heap * hp, int index)
             bestIndex = rightIndex;
 
         // Check if a swap is needed
-        if (bestIndex != index)
-        {
+        if (bestIndex != index) {
             swap(hp, bestIndex, index);
             swapped = 1;
             index = bestIndex;
@@ -282,8 +272,7 @@ void percolateDown(Heap * hp, int index)
 
 void append(Heap * hp, Shipment value) {
     // Check if the array was full
-    if (hp->size == hp->cap)
-    {
+    if (hp->size == hp->cap) {
         hp->cap *= 2;
         hp->array = (Shipment *) realloc(hp->array, sizeof(Shipment) * hp->cap);
     }
@@ -314,16 +303,14 @@ int isEmpty(Heap * hp) {
     return (hp->size == 0);
 }
 
-int update(Heap * hp, int oldTime, int newTime, double consumeRate)
-{
+int update(Heap * hp, int oldTime, int newTime, double consumeRate) {
     double consumptionTime, finishTime;
 
     // Keep track of the current time
     int curTime = oldTime;
 
     // Loop while there is some value in the heap
-    while (!isEmpty(hp) && curTime < newTime)
-    {
+    while (!isEmpty(hp) && curTime < newTime) {
         // Determine the time required to finish consuming the current shipment 
         consumptionTime = hp->array[0].mass / consumeRate;
 
@@ -335,15 +322,17 @@ int update(Heap * hp, int oldTime, int newTime, double consumeRate)
         if (finishTime > hp->array[0].expiration)
             finishTime = hp->array[0].expiration;
 
-        // Check if we can finish the shipment before the end of the update
-        if (finishTime <= newTime)
-        {
+        // If the shipment can be finished before the end of the
+        // update, consume it and move to next shipment in heap
+        if (finishTime <= newTime) {
             // Update time and remove from heap
             curTime = finishTime;
             dequeue(hp);
         }
-        else
-        {
+
+        // Otherwise, consume the part of the shipment that would be eaten from current
+        // time to end of update at the guessed consumption rate and complete update
+        else {
             // Update the remaining mass of the shipment and stop the simulation
             hp->array[0].mass -= consumeRate * (newTime - curTime);
             return 1;
@@ -354,9 +343,8 @@ int update(Heap * hp, int oldTime, int newTime, double consumeRate)
     return (curTime == newTime);
 }
 
-int canDo(Shipment * array, int numShipments, double consumeRate, int start, int end)
-{
-    // Create a heap used as a priority queue
+int canDo(Shipment * array, int numShipments, double consumeRate, int start, int end) {
+    // Create a heap used as a priority queue for shipments that the crew has
     Heap * pq = createHeap();
 
     // Add first shipment to priority queue
@@ -366,8 +354,7 @@ int canDo(Shipment * array, int numShipments, double consumeRate, int start, int
     int completedUpdate;
     
     // Loop through the remaining shipments
-    for (int i = 1; i < numShipments; i++)
-    {
+    for (int i = 1; i < numShipments; i++) {
         // Update the heap based on the arrival of the current shipment
         completedUpdate = update(pq, curTime, array[i].arrival, consumeRate);
         curTime = array[i].arrival;
