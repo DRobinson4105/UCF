2f21fe4b5bac23c0133dae3fad600dcf879dc3bc
DRobinson4105 <drobinson4105@gmail.com>
2023-05-30 18:53:32 -0400

    Create sqfree.cpp

---
+++ CompetitionPractice/UCFLocal/sqfree.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, a, b;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a >> b;
+        cout << (b - a - ceil(sqrt(b) - sqrt(a)) + 1) << endl;
+    }
+    return 0;
+}
