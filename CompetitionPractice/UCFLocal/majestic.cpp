1cdb3474b5e71c1fe0965ff97b642c95ac897c49
DRobinson4105 <drobinson4105@gmail.com>
2023-05-31 19:46:26 -0400

    More UCF Local Practice

---
+++ CompetitionPractice/UCFLocal/majestic.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n1, n2, n3;
+    cin >> n1 >> n2 >> n3;
+
+    int count = 0;
+    if (n1 >= 10)
+        count++;
+    if (n2 >= 10)
+        count++;
+    if (n3 >= 10)
+        count++;
+
+    vector<string> res = {"zilch", "double", "double-double", "triple-double"};
+
+    cout << res[count] << endl;
+    
+    return 0;
+}
