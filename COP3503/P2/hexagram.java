3887146eb9bfd8f4f9212aa730a8597aaa9b4b89
David Robinson <drobinson4105@gmail.com>
2024-01-16 15:49:26 -0500

    Revised COP3503 P2 Assignment

--- COP3503/P2/hexagram.java
+++ COP3503/P2/hexagram.java
@@ -2,16 +2,18 @@ import java.util.*;
 import java.io.*;
 
 /* 
-1 3 6 8     - 0 2 5 7
-1 4 7 11    - 0 3 6 10
-2 3 4 5     - 1 2 3 4
-2 6 9 12    - 1 5 8 11
-8 9 10 11   - 7 8 9 10
-5 7 10 12   - 4 6 9 11
+The six rows in the hexagram:
+- 0 2 5 7
+- 0 3 6 10
+- 1 2 3 4
+- 1 5 8 11
+- 7 8 9 10
+- 4 6 9 11
  */
 
 public class hexagram {
     private static int compute(int res[], int nums[], boolean used[], int curr, int target) {
+        // Check if the hexagram is now invalid
         if (
             (curr == 5 && res[1] + res[2] + res[3] + res[4] != target) ||
             (curr == 8 && res[0] + res[2] + res[5] + res[7] != target) ||
@@ -21,10 +23,12 @@ public class hexagram {
             (curr == 12 && res[4] + res[6] + res[9] + res[11] != target)
         ) return 0;
         
+        // Check if hexagram is complete
         if (curr == 12) return 1;
 
         int total = 0;
         
+        // Find all possible hexagrams using the current hexagram
         for (int i = 0; i < 12; i++) {
             if (used[i]) continue;
             used[i] = true;
@@ -37,25 +41,30 @@ public class hexagram {
     }
     
     public static void main(String[] args) {
-        // BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
         Scanner scan = new Scanner(System.in);
-
         int nums[] = new int[12];
+        boolean used[] = new boolean[12];
+        int res[] = new int[12];
 
         while ((nums[0] = scan.nextInt()) != 0) {
             int target = nums[0];
             for (int i = 1; i < 12; i++)
                 target += nums[i] = scan.nextInt();
             
-            if (target % 3 != 0) {
+            target *= 2;
+
+            // Sum of all rows is 2 * (sum of all values), if the sum of all rows
+            // can't be divided by 6, then the rows will never have equal values
+            if (target % 6 != 0) {
                 System.out.println(0);
                 continue;
             }
-            target /= 3;
-            boolean used[] = new boolean[12];
-            for (int i = 0; i < 12; i++) used[i] = false;
-            int res[] = new int[12];
 
+            target /= 6;
+            for (int i = 0; i < 12; i++) used[i] = false;
+            
+            // Divide result by 12 to remove reflections and rotations
+            // since there is 12 ways to display a hexagrams
             System.out.println(compute(res, nums, used, 0, target) / 12);
         }
     }
